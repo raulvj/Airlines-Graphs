d590716e03f212c4901736c781e3c0cdc9954395
stan <1994wangning@gmail.com>
2017-11-22 14:12:11 +0100

    add process data

--- src/ProcessData.java
+++ src/ProcessData.java
@@ -1,2 +1,36 @@
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ProcessData {
+    public static void main(String[] args) {
+        String pattern = "\"(Spain|Italy|Germany|France)\"";
+
+        Pattern r = Pattern.compile(pattern);
+
+        try {
+            FileInputStream fstream = new FileInputStream("airports.dat");
+            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+            FileOutputStream outputStream = new FileOutputStream("processd.dat");
+            String strLine;
+
+            while ((strLine = br.readLine()) != null)   {
+                // Print the content on the console
+                Matcher m = r.matcher(strLine);
+                if(m.find() && m.groupCount() != 0){
+                    System.out.println(strLine);
+                    outputStream.write((strLine+"\n").getBytes(Charset.forName("UTF-8")));
+                }
+//                System.out.println (strLine);
+            }
+
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
