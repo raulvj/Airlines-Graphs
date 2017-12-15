e738d296d0ecef83478325e3d1e15fdb47a20ee2
TheRoboNerd01 <therobonerd01@gmail.com>
2017-12-16 00:19:23 +0100

    airports.dat and routes.dat values corrected and get needed values for the graph. Graph still not implemented.

--- src/ProcessData.java
+++ src/ProcessData.java
@@ -5,32 +5,36 @@ import java.util.regex.Pattern;
 
 public class ProcessData {
     public static void main(String[] args) {
-        String pattern = "\"(Spain|Italy|Germany|France)\"";
-
-        Pattern r = Pattern.compile(pattern);
-
+        //String pattern = "\"(Spain|Italy|Germany|France)\"";
+        //Pattern r = Pattern.compile(pattern);
+        BufferedReader in = null;
         try {
-            FileInputStream fstream = new FileInputStream("airports.dat");
-            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
-            FileOutputStream outputStream = new FileOutputStream("processd.dat");
-            String strLine;
-
-            while ((strLine = br.readLine()) != null)   {
-                // Print the content on the console
-                Matcher m = r.matcher(strLine);
-                if(m.find() && m.groupCount() != 0){
-                    System.out.println(strLine);
-                    outputStream.write((strLine+"\n").getBytes(Charset.forName("UTF-8")));
+            in = new BufferedReader(new FileReader("src/airports.dat"));
+            String read = null;
+            while ((read = in.readLine()) != null) {
+                String[] splited = read.split(",");
+                if (splited[3].equals("\"Spain\"")|| splited[3].equals("\"Italy\"") || splited[3].equals("\"France\"") || splited[3].equals("\"Germany\""))
+                {
+                    //FIXME Still need to implement the node adding.
+                    //Use the openflights ID value for the airport identifier.
+                    System.out.println(splited[1]+" "+splited[0]);
                 }
-//                System.out.println (strLine);
             }
-
-        }catch (FileNotFoundException e){
-            e.printStackTrace();
+            in = new BufferedReader(new FileReader("src/routes.dat"));
+            while ((read = in.readLine()) != null) {
+                String[] splited = read.split(",");
+                //FIXME Still need to check if origin and arrival nodes exist and create the edge.
+                //Use the openflights ID value.
+                System.out.println("Origin: "+splited[3]+", Arrival: "+splited[5]);
+            }
         } catch (IOException e) {
+            System.out.println("There was a problem: " + e);
             e.printStackTrace();
+        } finally {
+            try {
+                in.close();
+            } catch (Exception e) {
+            }
         }
-
-
     }
 }
